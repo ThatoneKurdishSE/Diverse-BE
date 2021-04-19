9c6923bbcb4fc919b817df4de70c4cec4b34e7dc
Jack Hubert <jphubert93@gmail.com>
2021-04-19 12:56:35 -0600

    created community controller actions

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,2 +1,27 @@
 class CommunitiesController < ApplicationController
+
+    def index
+        @communities = Community.all
+
+        render json: @communities
+    end
+
+    def show
+        @community = Community.find(params[:id])
+
+        render json: @community
+    end
+
+    def create
+        @community = Community.create(community_params)
+
+        render json: @community, message: "Community Created!"
+    end
+
+    private
+
+    def community_params
+        params.require(:community).permit(:name, :location, :description)
+    end
+
 end
