1294b6c3d7d69dcb15f5c5807b1e25482af19334
Jack Hubert <jphubert93@gmail.com>
2021-04-19 14:26:50 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@ Community.destroy_all
 @stephen = User.create name: "Stephen", age: 197, email: "stephen@gjeffgolden.com", password: "password"
 @raz = User.create name: "Razio", age: 19, email: "raz@gjeffgolden.com", password: "password"
 @jeff = User.create name: "Jeff", age: 999, email: "jeff@gjeffgolden.com", password: "password"
+@obinna = User.create name: "Obinna", age: 23, email: "obinna@gjeffgolden.com", password: "password"
 
 @hiking = Community.create name: "Hiking", location: "Denver, CO", description: "A small community of hikers around Denver"
 @concerts = Community.create name: "Concerts", location: "Los Angeles, CA", description: "A medium-sized community of concert-goers around Los Angeles"
 @board_games = Community.create name: "Board Games", location: "Seattle, WA", description: "A large community of board game players around Seattle"
+@jazzercise = Community.create name: "Jazzercise", location: "Portland, OR", description: "A large community of board game players around Seattle"
 
 UserCommunity.create user: @jack, community: @hiking
 UserCommunity.create user: @jack, community: @board_games
@@ -18,9 +20,13 @@ UserCommunity.create user: @jack, community: @concerts
 UserCommunity.create user: @ryan, community: @concerts
 UserCommunity.create user: @ryan, community: @board_games
 UserCommunity.create user: @raz, community: @hiking
+UserCommunity.create user: @raz, community: @jazzercise
 UserCommunity.create user: @raz, community: @board_games
 UserCommunity.create user: @stephen, community: @hiking
 UserCommunity.create user: @stephen, community: @concerts
 UserCommunity.create user: @stephen, community: @board_games
 UserCommunity.create user: @jeff, community: @hiking
 UserCommunity.create user: @jeff, community: @concerts
+UserCommunity.create user: @obinna, community: @board_games
+UserCommunity.create user: @obinna, community: @concerts
+UserCommunity.create user: @obinna, community: @jazzercise
