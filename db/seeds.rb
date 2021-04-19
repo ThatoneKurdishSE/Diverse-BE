283da09515c85e9005b2f3d2dc01687abd29d619
Jack Hubert <jphubert93@gmail.com>
2021-04-19 13:45:56 -0600

    created seeds to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+UserCommunity.destroy_all
+User.destroy_all
+Community.destroy_all
+
+@jack = User.create name: "Jack", email: "jack@gjeffgolden.com", password: "password"
+@ryan = User.create name: "Ryan", email: "ryan@gjeffgolden.com", password: "password"
+@stephen = User.create name: "Stephen", email: "stephen@gjeffgolden.com", password: "password"
+@raz = User.create name: "Razio", email: "raz@gjeffgolden.com", password: "password"
+@jeff = User.create name: "Jeff", email: "jeff@gjeffgolden.com", password: "password"
+
+@hiking = Community.create name: "Hiking", location: "Denver, CO", description: "A small community of hikers around Denver"
+@concerts = Community.create name: "Concerts", location: "Los Angeles, CA", description: "A medium-sized community of concert-goers around Los Angeles"
+@board_games = Community.create name: "Board Games", location: "Seattle, WA", description: "A large community of board game players around Seattle"
+
+UserCommunity.create user: @jack, community: @hiking
+UserCommunity.create user: @jack, community: @board_games
+UserCommunity.create user: @jack, community: @concerts
+UserCommunity.create user: @ryan, community: @concerts
+UserCommunity.create user: @ryan, community: @board_games
+UserCommunity.create user: @raz, community: @hiking
+UserCommunity.create user: @raz, community: @board_games
+UserCommunity.create user: @stephen, community: @hiking
+UserCommunity.create user: @stephen, community: @concerts
+UserCommunity.create user: @stephen, community: @board_games
+UserCommunity.create user: @jeff, community: @hiking
+UserCommunity.create user: @jeff, community: @concerts
