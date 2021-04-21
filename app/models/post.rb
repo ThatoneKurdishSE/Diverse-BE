88f434570a3199a784bb2da0892da5f23c3b455b
Jack Hubert <jphubert93@gmail.com>
2021-04-21 11:55:22 -0600

    updated 'post' model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 1, maximum: 750 }
+    validates :ip_address, presence: true
+
+    belongs_to :users
+    belongs_to :communities
+    t.timestamps
 end
