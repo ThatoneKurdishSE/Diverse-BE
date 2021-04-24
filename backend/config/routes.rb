489320c1202ec044ee1c15eff63a1eee6c1f3dbb
rchoe1229 <ryan.choe@outlook.com>
2021-04-23 21:09:12 -0600

    Added route to user_communities

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   resources :community_owners
-  resources :comments, only: [:show, :create, :destroy]
-  resources :post_likes, only: [:show, :create, :destroy]
-  resources :post_tags, only: [:show, :create, :destroy]
+  resources :comments, only: %i[show create destroy]
+  resources :post_likes, only: %i[show create destroy]
+  resources :post_tags, only: %i[show create destroy]
   resources :posts
   resources :user_communities
   resources :communities
   resources :users
 
   # Custom Routes
-  post "/users", to: "users#create"
-  post "/login", to: "users#login"
-  get "/profile", to: 'users#profile'
-  get "/community_search", to: 'communities#search'
-  get "/user_search", to: 'users#search'
-  get "/community_members", to: 'communities#community_members'
-  get "/user_communities", to: 'users#user_communities'
+  post '/users', to: 'users#create'
+  post '/login', to: 'users#login'
+  post '/user_communities', to: 'users#user_communities'
+  get '/profile', to: 'users#profile'
+  get '/community_search', to: 'communities#search'
+  get '/user_search', to: 'users#search'
+  get '/community_members', to: 'communities#community_members'
+  get '/user_communities', to: 'users#user_communities'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
